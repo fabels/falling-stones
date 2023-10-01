8180d15bd6cc7b7bcae1974600ff98c8101f523d
Fabian Els <fabian.els@outlook.com>
2023-10-01 12:19:32 +0200

    registry.if-then-els.de

--- build.bash
+++ build.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 version=$(cat package.json | jq '.version' | sed "s/\"//g")
 
-docker build -t gummelhummel/falling-stones:${version} -t gummelhummel/falling-stones:latest .
+docker build -t registry.if-then-els.de/falling-stones:${version} -t registry.if-then-els.de/falling-stones:latest .
+docker push registry.if-then-els.de/falling-stones:${version}
+docker push registry.if-then-els.de/falling-stones:latest
